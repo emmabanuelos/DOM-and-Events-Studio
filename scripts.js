91a77c4ef9ff18a9d2d0e3aab884cd62d92fc5fa
Emma Banuelos <emmabanuelos@icloud.com>
2019-10-05 22:17:53 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function(load){
+    console.log("event type", event.type);
+   console.log("event target", event.target);
+});
